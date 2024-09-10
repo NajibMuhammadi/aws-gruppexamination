a02831a629e6ba0066d0e6a5475b6fae4583f5b3
Alistair McCann <allyjmccann@gmail.com>
2024-09-10 09:33:32 +0200

    Finished delete function and added BookingID control

--- functions/DeleteBooking/index.js
+++ functions/DeleteBooking/index.js
@@ -2,6 +2,7 @@ const { sendResponse, sendError } = require('../../responses/index')
 const { db } = require('../../services/index');
 
 exports.handler = async (event) => {
+
     //Ta värdet av id från pathParameters och tilldela det till en ny variabel som heter BookingID
     const { id: BookingID } = event.pathParameters;
 
@@ -10,21 +11,20 @@ exports.handler = async (event) => {
     }
 
     try {
-
+        //Kollar om bokningen finns i systemet.
         const result = await db.get({
-            TableName: 'test-bookings-db',
+            TableName: 'bookings-db',
             Key: { BookingID }
-        })
+        });
 
         if(!result.Item) {
             return sendError(404, { success: false, message: `No booking found with booking ID ${BookingID}` });
         }
-        
+
         await db.delete({
-            TableName: 'test-bookings-db',
+            TableName: 'bookings-db',
             Key: { BookingID: BookingID }
         });
-
         return sendResponse(200, { success: true, message: `Your booking with booking number ${BookingID} has been cancelled.` })
 
     } catch (error) {
